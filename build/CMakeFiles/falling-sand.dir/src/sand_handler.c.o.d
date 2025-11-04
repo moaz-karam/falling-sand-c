f23cc05574f518b7e684940629f245dd57197220
moaz-karam <moaz.karam.ali@gmail.com>
2025-11-04 17:17:12 +0200

    using rectangles instead of points

--- build/CMakeFiles/falling-sand.dir/src/sand_handler.c.o.d
+++ build/CMakeFiles/falling-sand.dir/src/sand_handler.c.o.d
@@ -39,7 +39,14 @@ CMakeFiles/falling-sand.dir/src/sand_handler.c.o: \
  /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
  /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
  /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
  /home/moaz/projects/falling-sand-c/src/../include/sand_handler.h \
  /home/moaz/projects/falling-sand-c/src/../include/./particle_handler.h \
  /usr/local/include/SDL3/SDL.h /usr/local/include/SDL3/SDL_stdinc.h \
